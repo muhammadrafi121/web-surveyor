6e80ad8c9b894e3f49cd6a9f04932fb049d4322d
faizmaruf <faiz.maruf8888@gmail.com>
2022-09-27 17:55:40 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -113,6 +113,7 @@
     @if ($script)
       <script src="/js/{{ $script }}.js"></script>
     @endif
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
 </body>
 
 </html>
